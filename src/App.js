921a5e8b495df25bddd17a490b32fe3864e919c6
AndyStan112 <stanica.andrei@gmail.com>
2022-02-09 10:42:06 +0200

    First Version
    Co-authored-by: ratasorin <ratasorin@users.noreply.github.com>
    Co-authored-by: broxaNaN <broxaNaN@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,183 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.png";
+import logoJB from "./mail.png";
+import para from "./para.png";
+import stanga from "./stanga.png";
+import dreapta from "./dreapta.png";
+import cDreapta from "./flower dreapta.png";
+import cStanga from "./flower stanga.png";
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import keys from "./keys";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [displayData, setDisplayData] = useState([]);
+
+  useEffect(() => {
+    //console.time();
+    init();
+    //console.timeEnd();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="topWrapper">
+        <img className="logo" src={logo}></img>
+
+        <div className="titles">
+          <div className="title">
+            <img className="timg" src={stanga}></img>
+            <p className="ttext">Clean up Challenge</p>
+            <img className="timg" src={dreapta}></img>
+          </div>
+          <div className="subtitle">
+            <img className="cimg" src={cStanga}></img>
+            <p className="ctext">Clasament</p>
+            <img className="cimg" src={cDreapta}></img>
+          </div>
+        </div>
+        <img className="logoJB" src={logoJB}></img>
+      </div>
+
+      <div className="bottomWrapper">
+        <div className="table">
+          <input
+            type="search"
+            className="row search"
+            placeholder="Search"
+            onKeyUp={search}
+            onChange={search}
+          ></input>
+          <div className="row header">
+            <button
+              onClick={sortBy}
+              className="rank cell button"
+              tabIndex={1}
+              id={"rank"}
+              title="Sort by rank"
+            >
+              Nr.
+            </button>
+            <button
+              onClick={sortBy}
+              className="name cell button"
+              tabIndex={2}
+              id={"name"}
+              title="Sort by name"
+            >
+              Nume
+            </button>
+            <button
+              onClick={sortBy}
+              className="score cell button"
+              tabIndex={3}
+              id={"score"}
+              title="Sort by score"
+            >
+              Scor
+            </button>
+          </div>
+          <Table table={displayData} />
+        </div>
+        <div className="para"></div>
+      </div>
+    </div>
+  );
+  async function init() {
+    const SPREADSHEET_ID = "1-745exi5JKWNBcM3n1j839mykXjLm7ypW8CYkV20G_8";
+    const SHEET_ID = "0";
+    const CLIENT_EMAIL = keys.client_email;
+    const PRIVATE_KEY = keys.private_key;
+    const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+    const input = document.getElementsByClassName("search");
+
+    try {
+      await doc.useServiceAccountAuth({
+        client_email: CLIENT_EMAIL,
+        private_key: PRIVATE_KEY,
+      });
+      await doc.loadInfo().then(async () => {
+        const sheet = doc.sheetsById[SHEET_ID];
+        await sheet.getRows().then((rows) => {
+          // create unique ids
+          const ids = new Set();
+          // make score number and add ids
+          const data = rows.map((row) => {
+            ids.add(row.name);
+            return {
+              ...row,
+              score: Number(row.score),
+            };
+          });
+
+          const finalData = Array.from(ids)
+            .map((id) => {
+              // A person's individual entries in the spreadsheet
+              const entriesForPerson = data.filter((row) => row.name === id);
+              // add final score
+              const score = entriesForPerson
+                .map((info) => info.score)
+                .reduce((prev, curr) => prev + curr, 0);
+              // produce one final info object per person
+              return {
+                ...entriesForPerson[0],
+                score,
+              };
+            })
+            .sort((curr, next) => (curr.score < next.score ? 1 : -1))
+            .map((info, i) =>
+              info.rank === undefined ? { ...info, rank: i + 1 } : info
+            );
+
+          setData(finalData);
+          setDisplayData(finalData);
+          // console.log(finalData);
+        });
+      });
+    } catch (e) {
+      console.error("Error: ", e);
+    }
+  }
+  function normalised(string) {
+    return string
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .toLowerCase();
+  }
+  function search(event) {
+    const query = normalised(event.currentTarget.value);
+    setDisplayData(
+      [...data].filter((row) => normalised(row.name).includes(query))
+    );
+  }
+
+  function sortBy(event) {
+    const method = event.currentTarget.id;
+    setDisplayData([...displayData].sort(getSortMethod(method)));
+  }
+  function getSortMethod(method) {
+    return (curr, next) => {
+      switch (method) {
+        case "score":
+          return curr.score < next.score ? 1 : -1;
+        case "name":
+          return curr.name > next.name ? 1 : -1;
+        default:
+          return curr.score < next.score ? 1 : -1;
+      }
+    };
+  }
+}
+function Table({ table }) {
+  return table.map((item, i) => <Row row={item} key={i} />);
+}
+function Row({ row }) {
+  return (
+    <div className="row">
+      <div className="rank cell">{row.rank}</div>
+      <div className="name cell">{row.name}</div>
+      <div className="score cell">{row.score}</div>
     </div>
   );
 }
---
+++ src/keys.js
@@ -0,0 +1,13 @@
+let keys={
+  "type": "service_account",
+  "project_id": "leaderboard-340607",
+  "private_key_id": "48700ed95fdabdbee538f140539d4a4aa260ee34",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDoGyn5oCExoAOb\ng7fqnpkcQT4Vuk4GX5x41lzYklvrYsxXoX6pQ7qGYPNDElZtk/5fj5td4yAc8cei\nRvvdxlUiylqL98IKx4U9TqGgLpzT6QFz76GN1t7LDf9Hf0qd58myXGK41itHkOTc\nyBfknqIiE9BGWm7dKoz+oiEpUD6voa3Hr1lPc2mWkSRUPfji6LNwXz60ow1+uJ0s\n1Bl10rf4klTVhNP/4Xn15UiQ6ZUiKf54kWgBd76PRSnP5WkQUpy10HcwoXPN8y+T\no7A8SPyMgIqD3HUMiX0a6+Dj4W6UUThqAUfaK0BubZv6HB/mjz8j+aIyHE7w1MYI\nZUcKRVVHAgMBAAECggEAEdhPeIW7MkQ0gsxfDjF2DBoSr+ZMVr5wb/SV3CtvLcBs\nKCtJFbJuBiQwum4P1F+V0GW2HZh3a5ebMFAiJSw5Ihz0q+Dh5k+YtYdKHd/3kW35\n/Uk7M9sv8zn279mgNFZnHMjFk6MZCL43GlJio+Jzt12r8advm/QdivuOw80Lmhe/\n9IwFS0pZKHu+dVelhhJ5g9Qj63CsuH9EEftnO/bgPM7qYtLgW5hJEMLweQbhDkna\ngtfzpRqY2u7AoU6VoFYH3DNK/6VYg8sSuzDGXxTw83A3dJWgnHwXgrqEGqCs03W5\nwxAZiVxNnxkxan+/lW90RB08AGCC2YClPdT/gfWHwQKBgQD6Kul96aAXtQ/tAI64\nMDp8hERSlTSu3vwpT7uU5sf/I3eNE1/o0Qe+wvJHxzYDu22WeOsPiNhQILMS0cSP\n7T1q+Woaq1MnkRP/rXksYhZWOnnO39mngdkKpAIMWv9J94/Xnor4WPearqj+sbPm\nvqTKmcVCfR0CiYTqLgSjOt83SQKBgQDthHRWLsTM015Qii2mkERr2Q2W/czuLdx3\nUqgAw29x/VGxYfP7c4JPO0gGeuG7APlViYRHr2FqOM2Fv8bwoYIdXhPBgurX3ZQi\npFhdhGcUuMzv1GRcCHOunL8TejYapbQJ1zj5DvYwUEGBhiEffS9mEJkIpLaDpak1\nJcwDCrBYDwKBgQCfjRrZ6VTnid0cZQyhQHEs1RbWLL8rAKCGoImckAviF3a6DS9Q\nR3DPI3pueQlkG/fqzjfs8nQztoc/a3YX7VEMlwIq/DXOuGKu4K61teFVIXpqjK4+\nfTw14yBDTQ4g2Llu5Xjwp51TlC3IMwHwGorzhFsg6rbD1X1l9e+TQ4r1sQKBgFJ4\nv3x9WNBWRFVq5VcEmP1prJJVJic9vWOhS7yYH8ZQ02DoRjN1dDXWu7ekS2UHfYv5\n5/iMKSMc/iAG6yj6q5dO50U2FB1ITvuIXTZ7lt9XG7Ha6c4bES/8NUyqDsdJ6i3Q\nkXPJgipcc6Jp1G5+pUqnULvzlXw4tXxC14fm2jx5AoGBALXluRPWhZRvCWTKAlxD\neB4Rt0VXdqFVfGN0afoeEGHYtMK1kekDe0uh9QoFH3jORDZjMc18AUZE2Rv0Jwy4\n3/niqig3dHPUQ86O+CyS0vxozcsSdlFfOABmk8Z5I/GyZxuHrKr1oH9xLfbwwWsf\nJm3jj4gfoPKpQc4bJ7v8/BiW\n-----END PRIVATE KEY-----\n",
+  "client_email": "junkbusters-leaderboard@leaderboard-340607.iam.gserviceaccount.com",
+  "client_id": "117284360307503242357",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/junkbusters-leaderboard%40leaderboard-340607.iam.gserviceaccount.com"
+}
+export default keys
